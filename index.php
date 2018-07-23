a201d9ec9f6b14c4085872d597ad33078fc552f9
Martynas Petruška <petruskamartynas@gmail.com>
2018-07-23 12:25:36 +0300

    Test git

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
     <title></title>
   </head>
   <body>
     <div class="container">
       Hello world!
+      <div class="head">
+        labas
+      </div>
+      <div class="body">
+      </div>
+      <div class="footer">
+      </div>
     </div>
   </body>
 </html>
